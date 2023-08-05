321663dab161b4122dae7869fcd4f4778e3f6010
Marnilyn Ramirez <You@example.com>
2023-08-04 23:12:55 -0400

    final edits

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
 var typeOfService = ["Synthetic Blend Oil - $44.99", "Full Synthetic Oil - $74.99", "Advanced Full Synthetic Oil - $99.99"]
-
-var service = document.getElementById("typeOfService")
-console(service)
